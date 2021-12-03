e53d6fcfbb1fb658673670267384ecb902397760
wangzhanbiao <2679009606@qq.com>
2021-12-03 16:44:20 +0800

    更新面向对象多继承多态代码

--- 面向对象/class06.py
+++ 面向对象/class06.py
@@ -148,12 +148,13 @@ class Person:
     def eat(self, *args, **kwargs):
         print(self.name + "正在吃饭...")
 
-    def run(self):
+    def run(self, *args, **kwargs):
         print(self.name + "正在跑步...")
 
 
 # 子类
 class Student(Person):
+    # 属性实例化
     def __init__(self, name, age, clazz):
         # 继承父类,将自己的name和age的值传给父类
         super().__init__(name, age)
@@ -164,10 +165,12 @@ class Student(Person):
 
     # 导致阴影是因为父类没有参数，而子类有参
     def eat(self, foold):
+        # 调用父类的方法
         super().eat()
         print(self.name + "吃的是{}".format(foold))
 
 
+# 子类
 class Employee(Person):
     def __init__(self, name, age, salary, manager):
         super().__init__(name, age)
@@ -175,6 +178,7 @@ class Employee(Person):
         self.manager = manager
 
 
+# 子类
 class Doctor(Person):
     def __init__(self, name, age, patients):
         # 判断属性是否属于Doctor类型
@@ -184,6 +188,7 @@ class Doctor(Person):
 
 # 对象
 s = Student('jack', 18, '2020')
+s.run()
 s.study('python基础')
 s.eat('鲍鱼龙虾面')
 # 对象
@@ -192,29 +197,299 @@ e = Employee('tom', 23, 10000, 'king')
 lists = ['zhangsan', 'lisi', 'wangwu', 'zhaoliu']
 d = Doctor('lucy', 30, lists)
 
-# class Animal:
-#     def run(self):
-#         print('Animal is running...')
-#
-#
-# class Dog(Animal):
-#     def run(self):
-#         print('Dog is running...')
-#
-#
-# class Cat(Animal):
-#     def run(self):
-#         print('Cat is running...')
-#
-#
-# # 子类和父类都存在相同的run()方法时，子类的run()覆盖了父类的run()，在代码运行的时候，总是会调用子类的run().
-# dog = Dog()
-# dog.run()
-#
-# cat = Cat()
-# cat.run()
-#
-#
+# 继承应用案例
+"""
+编写一个简单的工资管理程序，系统可以管理以下四类人：工人(worker)、销售员(salesman)、经理(manager)、销售经理(salemanger)
+所有员工都具有员工号、姓名、工资等属性，有设置姓名、获取姓名、获取员工号、计算工资等方法
+    1) 工人：工人具有工作小时数和时薪的属性，工资计算方法为工作小时数*时薪
+    2) 销售员：具有销售额和提成比例的属性，工资计算方法为销售额*提成比例
+    3) 经理：具有固定月薪的属性，工资计算方法为固定月薪
+    4) 销售经理：工资计算方法为销售额*提出比例+固定月薪
+请根据以上要求设计合理的类，完成以下功能：
+    1)添加所有类的人员  v
+    2)计算月薪
+    3)显示所有人的工资情况
+"""
+
+
+class System:
+    def __init__(self, id, name, salary):
+        self.id = id
+        self.name = name
+        self.salary = salary
+
+    # 设置姓名
+    def Sname(self, name):
+        self.name = name
+
+    # 获取信息
+    def __str__(self):
+        msg = "工号为: {},姓名叫做：{},固定月工资：{}".format(self.id, self.name, self.salary)
+        return msg
+
+    # 基本工资工资
+    def Csalary(self):
+        return self.salary
+
+
+# 工人
+class Worker(System):
+    def __init__(self, id, name, hours, hourly, salary):
+        super(Worker, self).__init__(id, name, salary)
+        self.hours = hours
+        self.hourly = hourly
+
+    # 工资计算方法为工作小时数*时薪
+    # 重写父类工资方法
+    def Csalary(self):
+        money = self.hours * self.hourly
+        self.salary += money
+        return self.salary
+
+
+# 销售员
+class Salesman(System):
+    def __init__(self, id, name, sales, ratio, salary):
+        super(Salesman, self).__init__(id, name, salary)
+        self.sales = sales
+        self.ratio = ratio
+
+    # 工资计算方法为销售额*提成比例
+    def Csalary(self):
+        money = self.sales * self.ratio
+        self.salary += money
+        return self.salary
+
+
+# 经理
+class Manager(System):
+    def __init__(self, id, name, msalary, salary):
+        super(Manager, self).__init__(id, name, salary)
+        self.msalary = msalary
+
+    # 工资计算方法为固定月薪
+    def Csalary(self):
+        money = self.msalary
+        self.salary += money
+        return self.salary
+
+
+# 销售经理
+class Salemanger(System):
+    def __init__(self, id, name, sales, ratio, msalary, salary):
+        super(Salemanger, self).__init__(id, name, salary)
+        self.sales = sales
+        self.ratio = ratio
+        self.msalary = msalary
+
+    # 工资计算方法为销售额*提出比例+固定月薪
+    def Csalary(self):
+        money = self.sales * self.ratio + self.msalary
+        self.salary = + money
+        return self.salary
+
+
+# 工人对象
+w = Worker('001', 'wangwu', 160, 100, 2000)
+w = w.Csalary()
+print("工资为：", w)
+
+# 销售员对象
+s = Salesman('002', 'lisi', 1500000, 0.03, 5000)
+s = s.Csalary()
+print('工资为：', s)
+
+# 经理对象
+m = Manager('003', 'zhangsan', 5000, 5000)
+m = m.Csalary()
+print('工资为：', m)
+
+# 销售经理对象
+sa = Salemanger('004', 'zhaosi', 1000000, 0.03, 50000, 25000)
+sa = sa.Csalary()
+print('工资为：', sa)
+
+
+# 继承应用
+class Animal:
+    def run(self):
+        print('Animal is running...')
+
+
+class Dog(Animal):
+    def run(self):
+        print('Dog is running...')
+
+
+class Cat(Animal):
+    def run(self):
+        print('Cat is running...')
+
+
+# 子类和父类都存在相同的run()方法时，子类的run()覆盖了父类的run()，在代码运行的时候，总是会调用子类的run().
+dog = Dog()
+dog.run()
+
+cat = Cat()
+cat.run()
+
+# 多继承与搜索顺序
+"""
+python允许多继承
+    def 子类(父类1，父类2,...)
+        pass
+        
+如果父类中有相同名称方法，搜索顺序：
+1.经典类(python2),深度优先，在python3环境中默认就是新式类。
+    深度是一条线走到底再走另一条
+2.新式类(python3),广度优先
+    广度是按层次走
+"""
+
+
+# 多继承
+class Base:
+    def test(self):
+        print("------> Base")
+
+
+class A(Base):
+    def test(self):
+        print("------> AAA")
+
+
+class B(Base):
+    def test(self):
+        print("------> BBB")
+
+
+# 多继承
+class C(Base):
+    def test(self):
+        print("------> CCC")
+
+
+class D(A, B, C):
+    pass
+
+
+# 对象
+d1 = D()
+d1.test()
+
+# 查看搜索顺序
+import inspect
+
+# getmro返回的是一个元组
+print(inspect.getmro(D))
+print(D.__mro__)
+
+
+# 搜索顺序
+# python3 从左至右，广度优先
+class P1:
+    def foo(self):
+        print("p1.foo")
+
+    def bar(self):
+        print("p1.bar")
+
+
+class P2:
+    def foo(self):
+        print("p2.foo")
+
+
+class C1(P1, P2):
+    pass
+
+
+class C2(P1, P2):
+    def bar(self):
+        print("C2.bar")
+
+
+class D(C1, C2):
+    pass
+
+
+# 对象
+d2 = D()
+d2.foo()
+d2.bar()
+print(D.__mro__)
+
+
+# 多态
+"""
+多态是指一类事务有多种形态。
+多态的好处就是，当我们传入Dog、Cat、Tortoise...时，只需要接收Person类型就可以了，因为Dog、Cat、Tortoise....都是Person类型。
+按照Person类型进行操作即可。由于Person类型有run()方法，因此传入的任意类型，只要是Person类或者子类，就会自定调用实际类型的run()方法，这就是多态
+
+对于一个变量，我们只需要知道它是Person类型，无需确切地知道它的子类型，就可以放心地调用run()方法，而具体调用的run()方法是作用在Person、Dog、Cat还是Tortoise对象上，由运行时该对象的确切类型决定，
+这就是多态真正的威力：调用方只管调用，不管细节，而当我们新增一种Animal的子类时，只要确保run()方法编写正确，不用管原来的代码是如何调用的。这就是著名的“开闭”原则
+
+若父类中存在私有化属性，则子类是无法直接继承父类的私有化属性
+
+"""
+
+
+# 人类
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def feed_pet(self, pet):
+        # 多态
+        if isinstance(pet, Cat):  # (object,type) type可以传入任意的类型，调用方只管调用即可
+            print("{}喜欢养宠物：{}".format(self.name, pet.role))
+        else:
+            print("不是宠物类型")
+
+
+# 宠物类
+class Pet:
+    role = "Pet"
+
+    def __init__(self, nickname, age):
+        self.nickname = nickname
+        self.age = age
+
+    def show(self):
+        print("昵称：{}，年龄：{}".format(self.nickname, self.age))
+
+
+# 猫类
+class Cat(Pet):
+    role = "猫"
+
+    def catch_mouse(self):
+        print("抓老鼠...")
+
+
+# 狗类
+class Dog(Pet):
+    role = "狗"
+
+    def watch_house(self):
+        print("看家高手...")
+
+
+# 虎类
+class Tiger:
+    def eat(self):
+        print("太可怕了，可以吃人")
+
+
+# 创建对象
+cat = Cat("花花", 2)
+dog = Dog("大黄", 4)
+person = Person("李四")
+person.feed_pet(cat)
+
+
+# 多态
+# 定义函数
 # def run_twice(animal):
 #     animal.run()
 #     animal.run()
@@ -225,4 +500,13 @@ d = Doctor('lucy', 30, lists)
 #         print('Tortoise is running ')
 #
 #
+# # 调用函数
 # run_twice(Tortoise())
+
+"""
+复习流程：
+    1.__init__
+    2.重写方法
+    3.多继承
+    4.多态
+"""
