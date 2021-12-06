2fab8953e570690efbb347e7c1cfb8970485f7f9
wangzhanbiao <2679009606@qq.com>
2021-12-06 10:11:25 +0800

    更新练习六代码

--- 面向对象/练习.py
+++ 面向对象/练习.py
@@ -137,3 +137,30 @@ print('Employee.__name__:', Employee.__name__)
 print('Employee.__module__:', Employee.__module__)
 print('Employee.__bases__:', Employee.__bases__)
 print('Employee.__dict__:', Employee.__dict__)
+
+
+# 练习六
+# 统计学生人数，可以给student类增加一个类属性，每创建一个实例，该属性自动增加：
+class Student:
+    count = 0
+
+    def __init__(self, name):
+        self.name = name
+
+        Student.count += 1
+
+
+# 测试:
+if Student.count != 0:
+    print('测试失败!')
+else:
+    bart = Student('Bart')
+    if Student.count != 1:
+        print('测试失败!')
+    else:
+        lisa = Student('Bart')
+        if Student.count != 2:
+            print('测试失败!')
+        else:
+            print('Students:', Student.count)
+            print('测试通过!')
