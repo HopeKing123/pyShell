3164b9b7f7fcc9abdb7eb9693a2c9d29cf79526c
wangzhanbiao <2679009606@qq.com>
2021-12-17 16:42:02 +0800

    更新模块代码部分

---
+++ 模块和包/模块/常用模块/Psutil.py
@@ -0,0 +1,38 @@
+﻿# psutil：跨平台对系统监控,支持Linux/OSX/Windows等，大多数用在Linux上
+import psutil
+"""
+获取CPU信息
+>>> psutil.cpu_count()  # CPU逻辑数量
+1
+>>> psutil.cpu_count(logical=False)  # CPU物理核心
+1
+
+统计CPU的用户/系统/空闲时间:
+>>> psutil.cpu_times()
+scputimes(user=273609.87, nice=1096.07, system=278325.64, idle=14621619.68, iowait=6167.03, irq=0.0, softirq=1127.34, steal=0.0, guest=0.0, guest_nice=0.0)
+
+# 实现类似于top命令的cpu使用率，每秒刷新一次，累计十次.
+>>> for x in range(10):
+...     print(psutil.cpu_percent(interval=1, percpu=True))
+
+
+获取内存信息
+使用psutil获取物理内存和交换内存信息，分别使用：
+>>> psutil.virtual_memory()
+svmem(total=2097086464, available=1003679744, percent=52.1, used=895107072, free=134893568, active=995336192, inactive=746115072, buffers=141500416, cached=925585408, shared=5808128, slab=166690816)
+>>> psutil.swap_memory()
+sswap(total=2047995904, used=311758848, free=1736237056, percent=15.2, sin=50343936, sout=419262464)
+
+返回的是字节为单位的整数，可以看到，总内存大小是2097086464 = 2GB 已用895107072 = 826M,
+交换分区大小是2047995904 = 20GB
+
+
+获取磁盘信息
+可以通过psutil获取磁盘分区，磁盘使用率和磁盘IO信息：
+>>> psutil.disk_partitions()    # 磁盘分区信息
+[sdiskpart(device='/dev/vda1', mountpoint='/', fstype='ext4', opts='rw,relatime,errors=remount-ro,data=ordered', maxfile=255, maxpath=4096)]
+>>> psutil.disk_usage('/')      # 磁盘使用情况
+sdiskusage(total=42140479488, used=13504188416, free=26686947328, percent=33.6)
+>>> psutil.disk_io_counters()   # 磁盘IO
+sdiskio(read_count=715655, write_count=22065466, read_bytes=27534892544, write_bytes=480235439104, read_time=1882768, write_time=27656920, read_merged_count=7498, write_merged_count=25945498, busy_time=9293272)
+"""
---
+++ 模块和包/模块/常用模块/Requests.py
@@ -0,0 +1,88 @@
+﻿# requests:用于处理url
+# requests常用方法：https://www.cnblogs.com/031602523liu/p/9817927.html
+import requests,json
+
+# get请求(无参)
+r = requests.get('https://bilibili.com/')
+print(r.status_code)
+# 响应返回文本内容
+# print(r.text)
+
+# get请求(有参),params：字典或字节序列，作为参数增加到url中
+r = requests.get('https://bilibili.com/', params={'q': 'python', 'cat': '1001'})
+print(r.url)
+# 检测编码
+print(r.encoding)
+
+# 无论响应是文本还是二进制内容，我们都可以用content属性获得bytes对象：
+# print(r.content)
+
+# 对特定类型响应,例如JSON
+# r = requests.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202151330&format=json')
+# print(r.json())
+
+# 需要传入HTTP Header时，传入一个dict作为headers参数：
+r = requests.get('https://www.bilibili.com/', headers={'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit'})
+# print(r.text)
+
+# POST请求,data:字典，字节序列或文件对象，作为Request的内容
+a = requests.post('https://mail.qq.com/', data={'form_email': 'xxxxxxx@qq.com', 'form_password': 'xxxxxxxxxx'})
+print(a)
+
+# requests默认使用application/x-www-form-urlencoded对POST数据编码。如果要传递JSON数据，可以直接传入json参数：
+params = ('key', 'value')
+b = requests.post('https://mail.qq.com', json=params)
+
+# 类似的，上传文件需要更复杂的编码格式，但是requests把它简化成files参数：
+upload_files = {'file': open('report.xls', 'rb')}
+c = requests.post('url', files=upload_files)
+
+# 在读取文件时，注意务必使用'rb'即二进制模式读取，这样获取的bytes长度才是文件的长度。
+# 把post()方法替换为put()，delete()等，就可以以PUT或DELETE方式请求资源。
+
+# requests获取HTTP响应的其他信息,例如：响应头
+print(r.headers)
+print(r.headers['Content-Type'])
+
+
+# requests对Cookie做了特殊处理,可以轻松获取指定的Cookie
+print(r.cookies['ts'])
+
+# 在请求中传入Cookie,只需要准备一个dict传入cokies参数
+cs = {'token': '12345', 'status': 'working'}
+r = requests.get('url', cookies=cs)
+
+# 指定超时，传入以秒为单位的timeout参数
+r = requests.get('url',timeout=2.5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
